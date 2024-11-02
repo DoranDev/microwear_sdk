9089e6eea34c5c29da355836e95ab2ec3f081d02
ZAKI MUBAROK <kakzaki@gmail.com>
2024-11-02 11:32:49 +0700

    init

--- lib/microwear_sdk.dart
+++ lib/microwear_sdk.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/services.dart';
+import 'package:microwear_sdk/microwear_device_control.dart';
 
 class MicrowearSdk {
   final methodChannel = const MethodChannel('microwear_sdk');
 
-  Future<String?> getPlatformVersion() async {
-    final version =
-        await methodChannel.invokeMethod<String>('getPlatformVersion');
+  Future<String?> sendRequest(MicrowearDeviceControl microwearDeviceControl,
+      {Map? data}) async {
+    final version = await methodChannel.invokeMethod('sendRequest',
+        {'microwearDeviceControl': microwearDeviceControl, 'data': data});
     return version;
   }
+
+  final EventChannel _deviceDataReceivedChannel =
+      const EventChannel('deviceDataReceived');
+  Stream get deviceDataReceivedStream => _deviceDataReceivedChannel
+      .receiveBroadcastStream(_deviceDataReceivedChannel.name)
+      .cast();
 }
